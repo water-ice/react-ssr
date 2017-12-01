1df06e25accf9b7cf910fd0af52e427369b79bd2
wanglei <wanglei@canopyu.com>
2017-12-01 11:37:25 +0800

    chore: add test file

---
+++ test/template.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './template.less'
+ReactDOM.render(
+	<div>
+		<h1>test for webpack1222 react environment</h1>
+	</div>,
+	document.getElementById('app')
+)
