1166341523faa24d4364e914d50412698819d11f
wangerniu <596864509@qq.com>
2017-12-03 00:32:01 +0800

    feat: make lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
 const TestStyle = {
-  color:"red",
-  textAlign:"center"
+  color: 'red',
+  textAlign: 'center',
 }
+
 export default class App extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h1 style={TestStyle}>Hello1222 React Server Render</h1>
       </div>
