301905ab361dd386e19e81c337f61a2576abd28c
wangerniu <596864509@qq.com>
2017-12-02 23:21:33 +0800

    chore: client config

--- src/entry_client.js
+++ src/entry_client.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AppContainer } from 'react-hot-loader';
+/**
+ * dev:使用ReactDOM.hydrate出问题
+ */
+// 挂载DOM节点
+const DOM = document.getElementById('App');
+/**
+ * 
+ * @param {Component}  React组件
+ * @function React组件绘制方法
+ * 
+ */ 
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>
+  ,DOM)
+}
+render(App);
 
-ReactDOM.hydrate(<App />,document.getElementById('app'))
+// 热刷新配置
+if(module.hot) {
+  module.hot.accept('./App.jsx',()=>{
+    const NextApp = require('./App.jsx').default;
+    render(NextApp)
+  })
+}
