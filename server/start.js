fa0f6b80f57d58924032ec8c3c37b88a5e30b7f4
wangerniu <596864509@qq.com>
2017-12-02 23:20:17 +0800

    chore: make judge in develop and productioin

--- server/start.js
+++ server/start.js
@@ -2,19 +2,35 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const ReactSSR = require('react-dom/server');
-const ServerEntry = require('../dist/entry_server').default;
 
 const app = express();
-const port = 3012;
-const template = fs.readFileSync(path.resolve(__dirname,'../dist/index.html'),'utf8')
+const port = 3333;
 
-// 静态资源
-app.use('/public',express.static(path.join(__dirname,'../dist')))
+const IsDev = process.env.NODE_ENV === 'develop';
 
-app.get("*",(request,response)=>{
-  const AppString = ReactSSR.renderToString(ServerEntry);
-  response.send(template.replace('<app></app>',AppString))
-})
+
+
+if(!IsDev) {
+  /**
+   * build构建执行代码
+   */
+  const ServerEntry = require('../dist/entry_server').default;
+  const template = fs.readFileSync(path.resolve(__dirname,'../dist/index.html'),'utf8')
+  
+  // 静态资源
+  app.use('/public',express.static(path.join(__dirname,'../dist')))
+
+  app.get("*",(request,response)=>{
+    // 使用ReactDOM
+    const AppString = ReactSSR.renderToString(ServerEntry);
+    
+    response.send(template.replace('<!-- app -->',AppString))
+  })
+}
+else {
+  const DevelopServer = require('./utils/dev-server')
+  DevelopServer(app)
+}
 
 app.listen(port,(err)=>{
   if(err) throw err;
