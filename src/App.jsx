e6ef0fee56ca919540a4e47fa0a20a427b59c47e
wangerniu <596864509@qq.com>
2017-12-03 20:34:18 +0800

    style: add icon and theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ComponentHeader from './components/header/header';
 import ComponentFooter from './components/footer/footer'
 import RouterMap from './config/router';
+
+import './static/fonts/iconfont.css'
+
+import './ui/theme.less';
+
 export default class App extends React.Component {
   render() {
     return [
