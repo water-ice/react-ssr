a02fd8ef6e3b8148cd2bca465f2716fc47ac9aa4
wangerniu <596864509@qq.com>
2017-12-03 19:27:58 +0800

    chore: server start file

--- server/start.js
+++ server/start.js
@@ -2,13 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const ReactSSR = require('react-dom/server');
-
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const ServerRender = require('./utils/server-render')
 const app = express();
 const port = 3333;
 
 const IsDev = process.env.NODE_ENV === 'develop';
 
+app.use(bodyParser.json())
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(favicon(path.join(__dirname,'../favicon.ico')))
+
+// app.use(session({
+// 	maxAge:10 * 60 * 1000,
+// 	name: "tid",
+// 	resave: false,
+// 	saveUninitialized:false,
+// 	secret: "wd"
+// }))
+
+
+app.use('/user/login',require('./utils/handle_login'))
+
+app.use('/api',require('./utils/proxy'))
 
 if(!IsDev) {
   /**
@@ -20,7 +40,7 @@ if(!IsDev) {
   // 静态资源
   app.use('/public',express.static(path.join(__dirname,'../dist')))
 
-  app.get("*",(request,response)=>{
+  app.get("*",(request,response,next)=>{
     // 使用ReactDOM
     const AppString = ReactSSR.renderToString(ServerEntry);
     
