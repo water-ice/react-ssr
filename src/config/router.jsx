41b3981bb3d99807e56059a518a5068e71576571
wangerniu <596864509@qq.com>
2017-12-03 19:35:06 +0800

    feat: some config in develop code

---
+++ src/config/router.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Route,
+  Redirect
+} from 'react-router-dom'
+
+import PageIndex from '../pages/index/index'
+import PageContest from '../pages/contest/index'
+import PageContestDetail from '../pages/contest_detail/index'
+
+import PageLogin from '../pages/account/login';
+import PageRegister from '../pages/account/register';
+
+export default () => [
+  <Route key="1" path="/" component={ PageIndex } exact/>,
+  <Route key="2" path="/contest" component={ PageContest } />,
+  <Route key="3" path="/contest_detail" component={ PageContestDetail } />,
+  <Route key="4" path="/account/" render={()=><Redirect to="/account/login" />} exact />,
+  <Route key="5" path="/account/login" component={PageLogin} />,
+  <Route key="6" path="/account/register" component ={PageRegister} /> 
+]
