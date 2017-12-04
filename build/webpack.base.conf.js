cd477988f114c2face3a3d325027d15092802f32
wanglei <wanglei@canopyu.com>
2017-12-04 20:15:54 +0800

    chore: base config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   output: {
     path: path.resolve(__dirname,'../dist'),
-    publicPath: '/public/'
+    publicPath: '/'
   },
   module:{
     rules: [
