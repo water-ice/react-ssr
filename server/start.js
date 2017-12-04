652ebf588457b2a338f90440b2ae63e754f88f08
wanglei <wanglei@canopyu.com>
2017-12-04 20:17:28 +0800

    chore: restart

--- server/start.js
+++ server/start.js
@@ -1,58 +1,17 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const ReactSSR = require('react-dom/server');
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const ServerRender = require('./utils/server-render')
+
+// webpack执行webpack.server.conf配置，打包出来的供给服务器运行的js
+const serverEntry = require('../dist/entry_server')
+
 const app = express();
-const port = 3333;
-
-const IsDev = process.env.NODE_ENV === 'develop';
-
-app.use(bodyParser.json())
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(favicon(path.join(__dirname,'../favicon.ico')))
-
-// app.use(session({
-// 	maxAge:10 * 60 * 1000,
-// 	name: "tid",
-// 	resave: false,
-// 	saveUninitialized:false,
-// 	secret: "wd"
-// }))
 
 
-app.use('/user/login',require('./utils/handle_login'))
+app.get("*",(req,res) =>{
+  const AppString =   ReactSSR.renderToString(serverEntry);
+  res.send(AppString);
+})
 
-app.use('/api',require('./utils/proxy'))
-
-if(!IsDev) {
-  /**
-   * build构建执行代码
-   */
-  const ServerEntry = require('../dist/entry_server').default;
-  const template = fs.readFileSync(path.resolve(__dirname,'../dist/index.html'),'utf8')
-  
-  // 静态资源
-  app.use('/public',express.static(path.join(__dirname,'../dist')))
-
-  app.get("*",(request,response,next)=>{
-    // 使用ReactDOM
-    const AppString = ReactSSR.renderToString(ServerEntry);
-    
-    response.send(template.replace('<!-- app -->',AppString))
-  })
-}
-else {
-  const DevelopServer = require('./utils/dev-server')
-  DevelopServer(app)
-}
-
-app.listen(port,(err)=>{
-  if(err) throw err;
-  console.log('server has running at '+port)
+app.listen(3333,()=>{
+    console.log('server has running at localhost: 3333')
 })
