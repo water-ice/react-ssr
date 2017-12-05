6d49cdde9c6f58534148cb093033525a1e6a59e6
wangerniu <596864509@qq.com>
2017-12-05 14:36:05 +0800

    chore: entry client

--- client/entry_client.js
+++ client/entry_client.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+// 热更新的container
+import {AppContainer} from 'react-hot-loader';
 
-ReactDOM.render(<App />,document.getElementById('app'))
+// 挂载DOM节点
+const DOM = document.getElementById('app')
+// render方法
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component/>
+        </AppContainer>,DOM)
+}
+// 渲染
+render(App)
+
+// 热更新
+if(module.hot){
+    module.hot.accept('./App.jsx',()=>{
+        // requre引用export出来的内容
+        const NextApp = require('./App.jsx').default;
+        // 重新渲染
+        render(NextApp)
+        
+    })
+}
