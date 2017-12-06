fec0b70ac7f183d8f88a51ddb054f67f2c871ad8
wangerniu <596864509@qq.com>
2017-12-06 17:32:24 +0800

    feat: add comemnts

--- server/start.js
+++ server/start.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+// 网站favicon处理
 const favicon = require('serve-favicon');
-const ReactSSR = require('react-dom/server');
-
-// 接口代理
+// 接口数据转化到res.body上
 const bodyParse = require('body-parser')
+// 服务器端session
 const session = require('express-session');
-// 环境变量
-const isDev = process.env.NODE_ENV === 'develop';
-
-
+// 服务器端渲染方法
+const SSR = require('./ssr')
+// 项目配置如:端口号，环境变量等
+const Config = require('../config/index')
+// express实例
 const app = express();
 
 // application/json -> req.body
@@ -28,14 +29,14 @@ app.use(session({
     secret:"test secret"
 }))
 
-// 浏览器上面的icon
+// 返回浏览器标题栏icon
 app.use(favicon(path.join(__dirname,'../favicon.ico')))
 
 // api拦截
 app.use('/api/user',require('./login-proxy'))
 app.use('/api',require('./proxy'))
 
-if(isDev) {
+if(Config.isDev) {
     // 开发环境
     const devServer = require('./dev-server');
     devServer(app)
@@ -44,19 +45,25 @@ else {
     // 生产环境
     
     // client/entry_server.js使用webpack打包后的，供给服务器渲染的react组件
-    const serverEntryFile = require('../dist/server_entry.js').default;
+    const serverEntryFile = require('../dist/server_entry.js');
     // client/index.html打包出来的页面
-    const serverTemplate = fs.readFileSync(path.join(__dirname,'../dist/index.html'),'utf8')
+    const serverTemplate = fs.readFileSync(path.join(__dirname,'../dist/server.ejs'),'utf-8')
     // 所有以public开头的请求，都以静态资源的形式返回
     app.use('/public',express.static(path.join(__dirname,'../dist')))
+
     // 所有请求都进入这里
-    app.get("*",(req,res)=>{
-        // 服务器端渲染号的react组件
-        const appString = ReactSSR.renderToString(serverEntryFile);
-        // 插入模板并返回
-        res.send(serverTemplate.replace('<!--app-->'),appString)
+    app.get("*",(req,res,next)=>{
+        // 交付给服务器端渲染方法
+        SSR(serverEntryFile,serverTemplate,req,res)
+        .catch(next)
+
     })
-    app.listen(3333,function(){
-        console.log('success: server has running at localhost:3333')
+    app.listen(Config.port.product,function(){
+        console.log('服务器已经启动成功！端口号:'+Config.port.product)
     })    
 }
+// 全局的错误处理机制
+app.use(function(err,req,res,next){
+    console.log(err);
+    res.status(500).send(err)
+})
