f91ebc6cbebe22275ec842aa2e621b84dbe02478
wangerniu <596864509@qq.com>
2017-12-06 17:29:48 +0800

    feat: page edit

--- client/pages/battle/index.jsx
+++ client/pages/battle/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet'
 export default class PageContest extends React.Component {
   constructor(props){
     super(props)
@@ -6,6 +7,11 @@ export default class PageContest extends React.Component {
   render(){
     return(
       <main style={{"minHeight":"500px","textAlign":"center","background":"#D1EEEE"}}>
+        <Helmet>
+          <title>
+            所有赛事
+          </title>
+        </Helmet>
         <h1>这里是赛事页面</h1>
       </main>
     )
--- client/pages/index/index.jsx
+++ client/pages/index/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import axios from 'axios';
-
+import { observer, inject } from 'mobx-react'
+import Helmet from 'react-helmet'
+// import {DemoState} from './store/demo_state'
+// 在provider上定义的叫啥名字，在这里inject就用啥名字
+// 使用demoState中的所有的数据和方法
+@inject('demoState') @observer
 export default class PageIndex extends React.Component {
   constructor(){
     super()
   }
+  changeName(ev){
+    // console.log(this.props)
+    this.props.demoState.changeName(ev.target.value);
+  }
+  asyncBootstrap() {
+    return new Promise((resolve)=>{
+      setTimeout(() => {
+        this.props.demoState.count = 3;
+        // 必须是true
+        resolve(true)
+      }, 1000);
+    })
+  }    
   componentWillMount(){
 
   }
@@ -12,6 +30,13 @@ export default class PageIndex extends React.Component {
   render(){
     return (
       <main className="page-index" >
+        <Helmet>
+          <title>首页</title>
+        </Helmet>
+        <div>
+          <input type='text' onChange={this.changeName.bind(this)} />
+          <span>{this.props.demoState.msg}</span>
+        </div>      
         <h1>首页</h1> 
       </main>
     )
