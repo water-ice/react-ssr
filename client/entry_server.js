b2bb6021357636840a2a703244caf86e3911c289
wangerniu <596864509@qq.com>
2017-12-06 17:29:20 +0800

    chore: entry server

--- client/entry_server.js
+++ client/entry_server.js
@@ -4,7 +4,7 @@ import { StaticRouter } from 'react-router-dom';
 
 import { Provider, useStaticRendering } from 'mobx-react'
 
-import { createStoreMap } from './store/store'
+import { CreateStoreMap } from './store/store'
 
 // 让mobx在服务器端渲染的时候，不会重复的数据变换
 useStaticRendering(true)
@@ -20,4 +20,4 @@ const render = (stores, routerContext, currentUrl) => (
 )
 export default render;
 
-export { createStoreMap }
+export { CreateStoreMap }
