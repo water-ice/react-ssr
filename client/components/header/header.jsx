968dabab2701d1bc25e019cefdc5a847cd26f789
wangerniu <596864509@qq.com>
2017-12-06 17:28:11 +0800

    feat: add link in header

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,44 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import { connect } from 'react-redux';
+import {Link} from 'react-router-dom'; 
 
-import LogoSrc from '@image/logo.png'
-// import '@style/component-header.less';
-
-class GlobalHeader extends React.Component {
+export default class ComponentHeader extends React.Component {
   constructor(props) {
     super(props)
-  }
-  Logout(){
-    this.props.dispatch({
-      type:"ClearUserInfo"
-    })
-  }
-  render(){
-    const IsLogin = this.props.user.name;
+  } 
+  render(){ 
     return (
       <header className="component-header">
-        <Link to='/'><img src={LogoSrc} /></Link>
-        <Link to="/"><span  className="icon iconfont">&#xe600;home1</span></Link>
-        <Link to="/contest"><span>Contest</span></Link>
-        
-        <div style={{"display":IsLogin?"none":"block"}}>
-          <Link to="/account/login"><span >Login</span></Link>
-          <Link to="/account/register"><span >Register</span></Link>
-        </div>
-        
-        <div style={{"display":IsLogin?"block":"none"}} >
-          <span>Your Name Is: {this.props.user.name}</span>
-          <span>Your Mobile Is: {this.props.user.mobile}</span>
-          <span onClick={this.Logout.bind(this)}>退出</span>
+       <div>
+          <Link to='/'>首页</Link>
+          <Link to='/battle'>全部赛事</Link>
+          <Link to='/battle_name'>征名比赛</Link>
+          <Link to='/battle_picture'>征图比赛</Link>
+          <Link to='/account'>账号</Link>
+          <Link to='/account/login'>登陆</Link>
         </div>
       </header>
     )
   }
 }
-const getUserInfoFromRedux = state => {
-  return {
-    user:state.USER_STATE
-  }
-}
-export default connect(getUserInfoFromRedux)(GlobalHeader)
