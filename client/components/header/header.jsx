9985752137996e808f74ed324d9c3477f1e8f077
wangerniu <596864509@qq.com>
2017-12-06 21:36:02 +0800

    style:make header style

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom'; 
 import '@style/component-header.less';
+import LogoSrc from '@image/logo.png'
 export default class ComponentHeader extends React.Component {
   constructor(props) {
     super(props)
@@ -8,14 +9,19 @@ export default class ComponentHeader extends React.Component {
   render(){ 
     return (
       <header className="component-header">
-       <div>
-          <Link to='/'>首页</Link>
-          <Link to='/battle'>全部赛事</Link>
-          <Link to='/battle_name'>征名比赛</Link>
-          <Link to='/battle_picture'>征图比赛</Link>
-          <Link to='/account'>账号</Link>
-          <Link to='/account/login'>登陆</Link>
-        </div>
+        <section className="main-container">
+            <Link to='/' className="logo-link">
+              <img src={LogoSrc} />
+            </Link>
+        <div>
+            <Link to='/'>首页</Link>
+            <Link to='/battle'>全部赛事</Link>
+            <Link to='/battle_name'>征名比赛</Link>
+            <Link to='/battle_picture'>征图比赛</Link>
+            <Link to='/account'>账号</Link>
+            <Link to='/account/login'>登陆</Link>
+          </div>
+        </section>
       </header>
     )
   }
