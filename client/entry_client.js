0c7248b8c1493efe71aad1c3dfe4a7f4e89e90d2
wangerniu <596864509@qq.com>
2017-12-06 09:52:12 +0800

    chore: rename temlate name

--- client/entry_client.js
+++ client/entry_client.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-// 热更新的container
-import {AppContainer} from 'react-hot-loader';
 
+// 热更新container
+import {AppContainer} from 'react-hot-loader';
+// 路由container
+import { BrowserRouter } from 'react-router-dom'
+// store container
+import { Provider } from 'mobx-react'
 // 挂载DOM节点
 const DOM = document.getElementById('app')
+
+import demoState from './store/demo_state'
+
+const inititalState = window.__INITIAL__STATE__ || new demoState();
 // render方法
 const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component/>
-        </AppContainer>,DOM)
+    ReactDOM.hydrate(
+        <AppContainer >
+            <Provider demoState={window.__INITIAL__STATE__.demoState}>
+                <BrowserRouter>   
+                    <Component/>
+                </BrowserRouter>
+            </Provider>
+        </AppContainer>
+    ,DOM)
 }
 // 渲染
 render(App)
--- client/entry_server.js
+++ client/entry_server.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import App from './App.jsx';
+import { StaticRouter } from 'react-router-dom';
 
-export default <App />;
+import { Provider, useStaticRendering } from 'mobx-react'
+
+import { createStoreMap } from './store/store'
+
+// 让mobx在服务器端渲染的时候，不会重复的数据变换
+useStaticRendering(true)
+
+// stores:{storeA:{},storeB:{}}
+
+const render = (stores, routerContext, currentUrl) => (
+    <Provider {...stores} >
+        <StaticRouter context={routerContext} location={currentUrl}>
+            <App />
+        </StaticRouter>
+    </Provider>
+)
+export default render;
+
+export { createStoreMap }
