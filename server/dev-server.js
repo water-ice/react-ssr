5eeb12a87e3e8c9d48eef0613a90f80e2f893a8f
wangerniu <596864509@qq.com>
2017-12-06 09:54:42 +0800

    chore: make development server config

--- server/dev-server.js
+++ server/dev-server.js
@@ -1,15 +1,19 @@
 const fs= require('fs');
+const ejs = require('ejs');
 const path = require('path');
 const axios = require('axios');
 const webpack = require('webpack')
 const MemoryFs = require('memory-fs');
 const ReactSSR = require('react-dom/server')
 const Proxy = require('http-proxy-middleware')
+const serialize = require('serialize-javascript')
+// 异步获取数据依赖
+const asyncBootstrap = require('react-async-bootstrapper').default;
 // dev server的时候是没有html页面的，所以使用axios去dev client下获取
 const getTemplateFromClient = () => {
     return new Promise((resolve,reject)=>{
-        // 此处暂时是死路径，可以写成活的
-        axios.get('http://localhost:8088/public/index.html')
+        // 此处暂时是死路径，可以写成活的,文件命名由webpack.client.conf决定
+        axios.get('http://localhost:8088/public/server.ejs')
         .then(res => {
             resolve(res.data)
         })
@@ -34,8 +38,10 @@ serverComplier.outputFileSystem = mfs;
 const Module = module.constructor;
 
 // 最终获得的内存中的bundle.js文件模块
-
 let ServerBundleModule;
+
+// 创建storeMap的方法，从entry_server中export出来的,和serverBundleModule一起赋值
+let CreateStoreMap ;
 
 // compiler监听文件的变动，生成新的bundle文件
 serverComplier.watch({}, (err,stats) => {
@@ -58,10 +64,20 @@ serverComplier.watch({}, (err,stats) => {
     const emptyModule = new Module();
     // js模块解析bundle的字符串，需要指定文件名
     emptyModule._compile(bundleString,'server-entry.js')
+    // console.log(emptyModule.exports)
     // 将解析后的js模块，赋值给外面的变量使用
     ServerBundleModule = emptyModule.exports.default;
-
+    // 获取到createStoreMap 方法
+    CreateStoreMap = emptyModule.exports.createStoreMap
 })
+
+const getStoreState = (stores) => {
+    return Object.keys(stores).reduce((result,storeName)=>{
+        result[storeName] = stores[storeName].toJson()
+        return result;
+    },{})
+}
+
 module.exports = function(app) {
 
     app.use('/public',Proxy({
@@ -71,12 +87,35 @@ module.exports = function(app) {
     app.get("*",function(req,res){
         // 先获取到template
         getTemplateFromClient().then(template => {
-            // 再获取到js模块
-            const content = ReactSSR.renderToString(ServerBundleModule);
-            // res返回
-            res.send(template.replace('<!--app-->',content))
+
+            const RouterContext = {};
+            // console.log(CreateStoreMap)
+            const stores =  CreateStoreMap()
+            // console.log(ServerBundleModule)
+            const app = ServerBundleModule(stores,RouterContext,req.url)
+
+            asyncBootstrap(app).then(()=>{
+                // 再获取到js模块
+                const content = ReactSSR.renderToString(app);
+                // 服务器端处理redirect，路由跳转
+                if( RouterContext.url){
+                    res.status(302).setHeader('Location', RouterContext.url);
+                    res.end();
+                    return;
+                }            
+                // 服务器端渲染后的数据
+                const state = getStoreState(stores)
+                console.log(stores.demoState)
+                console.log(state)
+                const html = ejs.render(template, {
+                    appString:content,
+                    initialState:serialize(state)
+                })
+                res.send(html)
+            })
         }).then(err => {
-            console.log(err)
+            // 监听获取模板的错误
+            // console.log(err)
         })
     })
     app.listen(3333,(err)=>{
