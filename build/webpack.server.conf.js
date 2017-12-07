ce6c45a935fe0a8cce70344f77e91b05ee20a5a0
wangerniu <596864509@qq.com>
2017-12-07 14:53:51 +0800

    chore: add extract-text-plugin in server

--- build/webpack.server.conf.js
+++ build/webpack.server.conf.js
@@ -2,7 +2,8 @@ const path = require('path');
 const WebpackMerge = require('webpack-merge')
 const baseConf = require('./webpack.base.conf')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-module.exports = WebpackMerge(baseConf,{
+const Config = require('../config/index')
+const ServerConfig = WebpackMerge(baseConf,{
     target:"node",
     entry: {
         "app":path.resolve(__dirname,'../client/entry_server.js')
@@ -16,3 +17,24 @@ module.exports = WebpackMerge(baseConf,{
     plugins:[
     ]
 })
+// 开发环境使用extract-text避免因为style-loader导致的服务器端渲染报错
+ServerConfig.module.rules.push( 
+    {
+        test:/\.css/,
+        use:ExtractTextPlugin.extract({
+        fallback:'style-loader',
+        use:'css-loader'
+        })
+    },   
+    {
+        test:/\.less/,
+        use:ExtractTextPlugin.extract({
+        fallback:'style-loader',
+        use:'css-loader!less-loader'
+        })
+    },          
+)
+ServerConfig.plugins.push(
+    new ExtractTextPlugin('css/main.[contenthash:8].css')
+)   
+module.exports = ServerConfig;
