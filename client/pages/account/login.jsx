231203f71d1b5e7d933e01a42eede7c7db5331c4
wangerniu <596864509@qq.com>
2017-12-07 10:59:00 +0800

    feat: remove login comoponent

--- client/pages/account/login.jsx
+++ client/pages/account/login.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
+import ComponentLogin from '@components/login/index';
 export default class PageLogin extends React.Component {
   constructor(){
     super()
   }
   render(){
     return(
-      <main style={{"background":"#6CA6CD","height":"500px","textAlign":"center"}}>
-        <input type="text" placeholder="手机"/>
-        <br />
-        <input type="password" placeholder="密码" />
-        <br />
-        <input type="button" value="登陆" />
-      </main>
+      <div>
+        <ComponentLogin/>
+      </div>
     )
   }
 }
