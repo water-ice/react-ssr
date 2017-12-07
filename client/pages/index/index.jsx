67f57e801e353fe8a766ae129edc18b2907eb2b7
wangerniu <596864509@qq.com>
2017-12-07 16:23:15 +0800

    style: test style

--- client/pages/index/index.jsx
+++ client/pages/index/index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import { observer, inject } from 'mobx-react'
 import Helmet from 'react-helmet' 
+import http from '../../config/http';
+
 @inject('demoState') @observer
+
 export default class PageIndex extends React.Component {
   constructor(){
     super()
@@ -12,12 +15,9 @@ export default class PageIndex extends React.Component {
     this.props.demoState.changeName(ev.target.value);
   }
   asyncBootstrap() {
-    return new Promise((resolve)=>{
-      setTimeout(() => {
-        this.props.demoState.count = 3;
-        // 必须是true
-        resolve(true)
-      }, 1000);
+    http.get('/api/contest')
+    .then(res => {
+      console.log(res)
     })
   }    
   componentWillMount(){
