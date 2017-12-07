138d1f157f005003f5e4679458e4e79880efcc57
wangerniu <596864509@qq.com>
2017-12-07 14:54:25 +0800

    style: remove battle and index style

--- client/pages/battle/index.jsx
+++ client/pages/battle/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet'
-import '@style/page-battle.less'
+import Helmet from 'react-helmet' 
 export default class PageContest extends React.Component {
   constructor(props){
     super(props)
