f85fe60e89dfd81e675d0c221bee00656b0bc013
wangerniu <596864509@qq.com>
2017-12-08 16:38:16 +0800

    chore: make proxy

--- server/proxy.js
+++ server/proxy.js
@@ -1,49 +1,21 @@
 // 全部代理
 const axios = require('axios');
-const qs = require('query-string')
-const baseUrl = 'http://cnodejs.org/api/v1';
+const qs = require('query-string') 
+const http = require('../config/http')
+const Config = require('../config/index')
+const apiDomain = Config.isApiDev ? Config.domain.dev:Config.domain.prod;
 
 module.exports = function(req,res,next){
-    const path = req.path;
-    const user = req.session.user || {};
-    // 判断该接口是否需要用户登陆
-    const needAccessToken = req.query.needAccessToken;
-    // 如何是需要用户登陆，但是服务器中的session中没有accessToken的话，直接返回
-    if(needAccessToken && user.accessToken) {
-        res.status(401).send({
-            success:false,
-            msg:"need login"
-        })
-    }
-    // 
-    const getParam = Object.assign({},req.query,{
-        accesstoken:(needAccessToken&& req.method === 'get')? user.accessToken: ''
-    })
-    const postParam = qs.stringify(Object.assign({},req.body,{
-        accesstoken:(needAccessToken&& req.method === 'post')? user.accessToken: ''
-    }))
-    // 
-    if(getParam.needAccessToken)  delete getParam.needAccessToken;
-    // qs的作用：{'key':'value} => 'key'='value'
-    // console.log(qs.stringify(Object.assign({},req.body,{
-    //     accesstoken:(needAccessToken&& req.method === 'post')? user.accessToken: ''
-    // })))
-    // console.log(req.body,'body')
-    // console.log(user,'user')
-    console.log(req.session,'session')
-    console.log(getParam);
-    console.log(postParam)
-    axios(`${baseUrl}${path}`, {
-        method: req.method,
-        params:getParam,
-        data:postParam,
-        headers: {
-            'Content-type':'application/x-www-form-urlencoded'
-        }
-    })
+    let path = req.path.replace('/api/',''); 
+    // console.log("当前API_ENV为: "+process.env.API_ENV)
+    // console.log('当前Ajax完整请求路径为：'+`${apiDomain}${path}`)
+
+    http.ajax(`${apiDomain}${path}`,req.method,'')  
     .then(resp => {
-        console.log('a')
-        if(resp.sttus == 200) {
+        console.log("代理返回为："+resp)
+        console.log("接口返回状态:"+resp.status)
+        console.log("返回数据为："+resp)
+        if(resp.status == 200) {
             res.send(resp.data)
         }
         else {
@@ -51,9 +23,8 @@ module.exports = function(req,res,next){
         }
     })
     .catch(err => {
-        console.log('b')
+        console.log('当前接口转发错误信息为:'+err)
         if(err.response) {
-            console.log(err.response.data)
             res.status(500).send(err.response.data)
         }
         else {
