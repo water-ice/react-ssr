6f645fb7b0d11d00b47be42174c9c83c70e8c7b7
wangerniu <596864509@qq.com>
2017-12-10 20:51:57 +0800

    feat: add data fetch

--- client/pages/battle/index.jsx
+++ client/pages/battle/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import Helmet from 'react-helmet' 
+import '@style/page-battle.less'
+
+import { observer, inject } from 'mobx-react'
+
+@inject("battleState") @observer
+
 export default class PageContest extends React.Component {
   constructor(props){
     super(props)
   }
-  render(){
+  asyncBootstrap() {    
+    return this.props.battleState.GetBattleList()
+    .then(()=>{
+      return true;
+    }) 
+    .catch(()=>{
+      return false;
+    })
+  }  
+  DeleteBattle(){
+    this.props.battleState.DeleteBattleList()
+  }
+  componentDidMount(){
+    this.props.battleState.GetBattleList()
+  }
+  render(){ 
     return(
       <main className="page-battle">
         <Helmet>
@@ -12,7 +33,16 @@ export default class PageContest extends React.Component {
             所有赛事
           </title>
         </Helmet>
-        <h1>这里是赛事页面23321</h1>
+        <button onClick={this.DeleteBattle.bind(this)}>删除一个</button>
+        <ul>
+          {
+            this.props.battleState.BattleList.map(item=>(
+              <li key={item.id}>
+                <p>{item.title}</p>
+              </li>
+            ))
+          }
+        </ul>
       </main>
     )
   }
