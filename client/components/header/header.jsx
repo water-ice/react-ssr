833d5225d28bc060d791130e4a699fa143dfeaa5
wangerniu <596864509@qq.com>
2017-12-16 22:04:46 +0800

    refactor: remove links in header

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'; 
 import '@style/component-header.less';
-import LogoSrc from '@image/logo.png'
 export default class ComponentHeader extends React.Component {
   constructor(props) {
     super(props)
@@ -11,23 +10,11 @@ export default class ComponentHeader extends React.Component {
       <header className="component-header">
         <section className="main-container clearfix">
           <Link to='/' className="module-logo">
-            <img src={LogoSrc} />
+            <img  />
           </Link>
           <div className="module-nav">
-            <Link className="nav-anchor" to='/'>首页</Link>
-            <Link className="nav-anchor" to='/battle'>赛事</Link>
-            <Link className="nav-anchor" to='/battle'>玩法</Link>
-            <Link className="nav-anchor" to='/battle'>观战</Link>
-            <Link className="nav-anchor" to='/battle'>活动</Link>
-            <Link className="nav-anchor" to='/battle'>排行</Link>
-            <Link className="nav-anchor" to='/battle'>了解精于</Link>
-            <Link className="nav-anchor" to='/cooperation'>商务合作</Link>
-            <Link className="nav-anchor" to='/battle'>我有需求</Link>
-          </div>
+            </div>
           <div className="module-account">
-            <Link to="/account">
-              <i className="icon iconfont">&#xe605;</i>
-            </Link>
           </div>
         </section>
       </header>
