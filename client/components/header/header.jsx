afb3937798bde9340d47f30b8ea2baff1ddbb254
wangerniu <596864509@qq.com>
2017-12-17 19:44:30 +0800

    style: header component

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import {Link} from 'react-router-dom'; 
 import '@style/component-header.less';
+import http from '../../../config/http'
 export default class ComponentHeader extends React.Component {
   constructor(props) {
     super(props)
   } 
+  componentDidMount(){
+    http.get('https://www.toutiao.com/stream/widget/local_weather/data/?city=北京')
+    .then(response => {
+      console.log(response)
+    })
+  }
   render(){ 
     return (
       <header className="component-header">
         <div className="header-container">
           <div className="left ">
             <a className="inline-block pl10 pr10 br-3a" href="http://app.toutiao.com/news_article/" target="blank">下载APP</a>
-            <span className="pl10">&nbsp;北京</span>
-            <span className="pl13 pr14">晴</span>
-            <span className="mr2">-8</span>°
+            <span className="pl7">&nbsp;北京</span>
+            <span className="pl12 pr14">晴</span>
+            <span className="">-7</span>°
             <span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>
-            <span className="mr2">0</span>°
+            <span className="mr2">5</span>°
             <i className="icon iconfont fs13 bold">&#xe6a6;</i>
           </div>
           <ul className="right">
-            <li className="left ph10 pr14 br-3a">
+            <li className="left br-3a">
               <Link to="/"><div className="login-btn">登录</div></Link>
             </li>
             <li className="left ph10 br-3a"><Link to="/"><span>反馈</span></Link></li>
-            <li className="left ph10 br-3a"><Link to="/"><span>投诉侵权</span></Link></li>
+            <li className="left ph10 br-3a"><Link to="/"><span>侵权投诉</span></Link></li>
             <li className="left ph10"><Link to="/"><span>头条产品</span></Link></li>
           </ul>
         </div>
