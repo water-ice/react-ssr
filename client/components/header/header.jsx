7abe1da7cddda7276d8a310925d11f6ba3a9f7e2
wangerniu <596864509@qq.com>
2018-01-11 19:17:17 +0800

    feat: component header

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,38 +1,83 @@
 import React from 'react';
-import {Link} from 'react-router-dom'; 
-import '@style/component-header.less';
-import http from '../../../config/http'
+import { Link,NavLink} from 'react-router-dom'; 
+import { inject , observer} from 'mobx-react';
+
+import '@style/component_header.less';
+
+import http from '@clientConfig/http';
+import Storage from '@utils/storage'
+
+@inject((stores)=>{
+  return {
+      UserStore:stores.UserStore
+  }
+}) @observer
+
 export default class ComponentHeader extends React.Component {
   constructor(props) {
     super(props)
   } 
-  componentDidMount(){
-    http.get('https://www.toutiao.com/stream/widget/local_weather/data/?city=北京')
-    .then(response => {
-      console.log(response)
+  Logout(){
+    http.post('/api/v1/login/loginOut')
+    .then(response => { 
+      if(response.data.status == 1) {
+        this.props.UserStore.clearUserInfo();
+        Storage.clear('user')
+        this.props.history.push('/user')
+      }
+      else {
+        alert(response.data.message.errorMsg)
+      }
     })
   }
+  goUser(method){
+    this.props.history.push({
+        pathname:'/user',
+        query:{
+            method
+        }
+    })
+}  
   render(){ 
     return (
       <header className="component-header">
-        <div className="header-container">
-          <div className="left ">
-            <a className="inline-block pl10 pr10 br-3a" href="http://app.toutiao.com/news_article/" target="blank">下载APP</a>
-            <span className="pl7">&nbsp;北京</span>
-            <span className="pl12 pr14">晴</span>
-            <span className="">-7</span>°
-            <span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>
-            <span className="mr2">5</span>°
-            <i className="icon iconfont fs13 bold">&#xe6a6;</i>
-          </div>
-          <ul className="right">
-            <li className="left br-3a">
-              <Link to="/"><div className="login-btn">登录</div></Link>
-            </li>
-            <li className="left ph10 br-3a"><Link to="/"><span>反馈</span></Link></li>
-            <li className="left ph10 br-3a"><Link to="/"><span>侵权投诉</span></Link></li>
-            <li className="left ph10"><Link to="/"><span>头条产品</span></Link></li>
+        <div className="main-container clearfix">
+          <Link to='/' className="inline-block">
+            <img width="140" height="34" className="logo" src={require('@image/header_logo.png')} />
+          </Link>
+          <div className="user">
+          {
+              (this.props.UserStore.user.isLogin) ?(
+                  <div className="login">
+                      <img width="30" height="30" alt={this.props.UserStore.user.info.name} src={this.props.UserStore.user.info.avatar} title={this.props.UserStore.user.info.name} />
+                      <div className="links abs">
+                          <a className="link" href="">个人中心</a>
+                          <a className="link" href="">信息</a>
+                          <a className="link" href="">设置</a>
+                          <a className="link hand" onClick={this.Logout.bind(this)} >退出</a>
+                      </div>
+                  </div>
+              ):(
+                <a onClick={this.goUser.bind(this,'login')} className="hand unlogin" ><span className="iconfont ">&#xe63d;</span></a>  
+              )
+          }
+          </div>        
+          <ul className="nav customer-nav">
+            <li><NavLink activeClassName="nav-active" to="/1">了解精于</NavLink></li>
+            <li><NavLink activeClassName="nav-active" to="/2">商业合作</NavLink></li>
+            <li><NavLink activeClassName="nav-active" to="/3">发布需求</NavLink></li>            
           </ul>
+          <ul className="nav designer-nav">
+            {/* <li><NavLink exact activeClassName="nav-active" to="/battle">赛事</NavLink></li> */}
+            {/* <li><NavLink activeClassName="nav-active" to="/work/submit/1">佳作</NavLink></li> */}
+            <li><a href="/battle">赛事</a></li>
+            <li><a href="/work/submit/1">佳作</a></li>
+            
+            <li><NavLink activeClassName="nav-active" to="/5">玩法</NavLink></li>
+            <li><NavLink activeClassName="nav-active" to="/6">观战</NavLink></li>
+            <li><NavLink activeClassName="nav-active" to="/7">活动</NavLink></li>
+            <li><NavLink activeClassName="nav-active" className="hr" to="/8">排行</NavLink></li>
+          </ul>            
         </div>
       </header>
     )
