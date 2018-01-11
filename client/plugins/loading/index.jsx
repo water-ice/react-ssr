589d6e160fd6f3fbbd644245cdeedda0c875720c
wangerniu <596864509@qq.com>
2018-01-11 19:15:08 +0800

    feat: loading plugin

---
+++ client/plugins/loading/index.jsx
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react';
+import "./loading.less";
+export default class PluginLoading extends Component {
+    render(){
+        return(
+            <div className="loading-wrapper">
+                <div className='loading-container'>
+                    <span className='loading-dot'></span>
+                    <span className='loading-dot'></span>
+                    <span className='loading-dot'></span>
+                </div>
+            </div>
+        )
+    }
+}
