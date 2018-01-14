3da8912e243e66eb882c176ba72a24e9d037e727
wangerniu <596864509@qq.com>
2018-01-14 22:29:49 +0800

    feat: storage util

---
+++ client/utils/storage.js
@@ -0,0 +1,14 @@
+
+export default {
+	get(key){
+		return JSON.parse(localStorage.getItem(key))
+	},
+	set(obj){
+		Object.keys(obj).forEach((item,index)=>{
+			localStorage.setItem(item, JSON.stringify(obj[item]))	
+		})
+	},
+	clear(){
+		localStorage.clear();
+	}
+}
