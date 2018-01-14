e8422cfd8a7f943c4b3e5f02cf50b0a129aa6923
wangerniu <596864509@qq.com>
2018-01-14 22:36:55 +0800

    feat: dev-server

--- server/dev-server.js
+++ server/dev-server.js
@@ -15,7 +15,7 @@ const Config = require('../config/index')
 const getTemplateFromClient = () => {
     return new Promise((resolve,reject)=>{
         // 此处暂时是死路径，可以写成活的,文件命名由webpack.client.conf决定
-        axios.get('http://localhost:8088/static/server.ejs')
+        axios.get('http://localhost:8087/static/server.ejs')
         .then(res => {
             resolve(res.data)
         })
