4f2a72d4203d13ec54fab11d28991b79a1223cec
wangerniu <596864509@qq.com>
2018-01-16 18:32:51 +0800

    feat: new message plugin

---
+++ client/plugins/message/index.jsx
@@ -0,0 +1,41 @@
+import React,{ Component } from 'react';
+
+import './message.less';
+
+const iconType = {
+    info:(<i className="iconfont message-info">&#xe8be;</i>),
+    success:(<i className="iconfont message-success">&#xe602;</i>),
+    error:(<i className="iconfont message-error">&#xe71a;</i>)
+} 
+
+class PluginMessage extends Component { 
+    constructor(props){
+        super(props);
+        this.state = {
+            content:""
+        }
+    }
+    info(content){
+        this.setState({
+            content
+        })
+    }
+    render(){
+        // const icon = iconType[type];
+                
+        return(
+            <div>
+                <div className="plugin-message">
+                    <span>      
+                    {
+                        this.state.content
+                    }                                                       
+                    </span> 
+                </div>   
+                
+            </div>   
+
+        )
+    } 
+} 
+export default PluginMessage;
