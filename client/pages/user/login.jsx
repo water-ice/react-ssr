9240b7822b3ccc2ed1d532b21f5b2677df48b1b2
wangerniu <596864509@qq.com>
2018-01-16 18:30:21 +0800

    feat: client pages

---
+++ client/pages/user/login.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Helmet from 'react-helmet';
+
+import ComponentHeader from '@components/header/header';
+import ComponentFooter from '@components/footer/footer';
+
+import absolteHttp from '@serverConfig/http'
+import relativeHttp from '@clientConfig/http';
+import '@style/user/page_login.less';
+
+class ComponentLogin extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			 
+		}
+	}   
+	test(){
+		absolteHttp.post('http://api.lips-gp.top/index/index/get_addr')
+		relativeHttp.post('/api/index/index/get_addr')
+	} 
+	render() { 
+ 
+		return (
+			<div className="page-login">
+				<Helmet>
+					<title>登录</title>
+				</Helmet>
+				<ComponentHeader />
+				<main className="container">	
+					<div className="text-center mt10">
+						<input className="input bg-fff" type="text" placeholder="账号"/>
+					</div>
+					<div className="text-center mt10">
+						<input className="input bg-fff" type="text" placeholder="密码"/>					
+					</div>	
+					<div className="text-center mt40">
+						<button onClick={this.test.bind(this)} className="btn">登录</button>
+					</div>
+				</main>
+				<ComponentFooter />
+			</div>
+		)
+	}
+}
+export default ComponentLogin;
