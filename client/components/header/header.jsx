e69250a642c2a836784cbac8a5e8c6c3720d1a7c
wangerniu <596864509@qq.com>
2018-01-17 13:53:30 +0800

    feat: add logout method

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -15,22 +15,38 @@ import http from '@clientConfig/http';
 export default class ComponentHeader extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      user:props.UserStore.user
+    }
   } 
+  componentDidMount(){
+    console.log(this.props.UserStore.user.name)
+  }
   Logout(){ 
+    http.post('/api/user/user/logout')
+    .then(response => {
+      if(response.data.status == 1) { 
+        this.props.UserStore.clearUserInfo();
+      }
+      else {
+        alert(response.data.message)
+      }
+    })
   } 
   render(){ 
-    const user = this.props.UserStore;
-    console.log(user)
+    const user = this.state.user || {};    
     return (
       <header className="component-header">
         <img className="logo" src="" alt="LOGO"/>
-        <div className="main">
-          	<nav></nav>
+        <div className="main"> 
 			{
-				(user.isLogin)?
+				(user.uid)?
 				(
-					<div className="user">
-						用户已经登录
+					<div className="user hand"> 
+            <img src={user.avatar} width="40" alt=""/>
+            <ul className="operate">
+              <li onClick={this.Logout.bind(this)}>退出</li>
+            </ul>
 					</div>  
 				):(
 					<div className="account">
